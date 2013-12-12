bc327bf5f655b4c554b98c405dee1208d6315f88
JonathanF <jonathanf@zillow.com>
2013-12-11 17:09:19 -0800

    Stuff

---
+++ d3-packages.js
@@ -0,0 +1,24 @@
+'d3-packages': {
+    'homevalues': [
+        'start-yui.js'
+        'arrays/max.js'
+        'arrays/min.js'
+        'format/format.js'
+        'geo/path.js'
+        'interpolate/interpolate.js'
+        'layout/pie.js'
+        'event/mouse.js'
+        'scale/category.js'
+        'scale/linear.js'
+        'selection/selection.js'
+        'svg/arc.js'
+        'svg/axis.js'
+        'svg/line.js'
+        'time/format.js'
+        'time/month.js'
+        'time/scale.js'
+        'time/year.js'
+        'transition/transition.js'
+        'end-yui.js'
+    ]
+}
